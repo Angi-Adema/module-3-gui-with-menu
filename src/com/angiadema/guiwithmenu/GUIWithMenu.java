4d542612310c767cc5bd97f3b51bb31c5ced1323
Angi-Adema <dr.adema@gmail.com>
2026-03-31 16:00:05 -0600

    added the TextArea and specified that it could not be edited, added the LocalDateTime functionality for the menu option 1

--- src/com/angiadema/guiwithmenu/GUIWithMenu.java
+++ src/com/angiadema/guiwithmenu/GUIWithMenu.java
@@ -1,12 +1,16 @@
 package com.angiadema.guiwithmenu;
 
-import java.awt.Color;
-import java.awt.Menu;
-import java.awt.MenuBar;
-import java.awt.MenuItem;
-import java.awt.TextArea;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -23,6 +27,10 @@ public class GUIWithMenu extends Application {
 		// Set the BorderPane
 		borderPane = new BorderPane();
 		
+		// Set TextArea
+		textArea = new TextArea();
+		textArea.setEditable(false);
+		
 		// Add a title to the GUI
 		stage.setTitle("GUI Menu App");
 		
@@ -37,8 +45,27 @@ public class GUIWithMenu extends Application {
 		MenuItem opt4 = new MenuItem("Exit");
 		
 		// MenuItem 1: Print Date and Time to TextArea
+		opt1.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				LocalDateTime now = LocalDateTime.now();
+				DateTimeFormatter formatter = 
+						DateTimeFormatter.ofPattern("MM-dd-yy HH:mm:ss");
+				textArea.setText("Current date and time: " + now.format(formatter));
+			}
+		});
+		
+		// MenuItem 2: Write TextArea content to log.txt
+		
+		// MenuItem 3: Change background color to a hue of green
+		
+		// MenuItem 4: Exit the program
 		
 		// Add all the menu items to the menu and add the menu to the menu bar
+		
+		
+		
+		
 		
 	}
 
